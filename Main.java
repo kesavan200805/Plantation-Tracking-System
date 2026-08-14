9c857c1e7dbc29dd2cb4afae85b5cef3afd549a2
kesavan200805 <kesavans20082005@gmail.com>
2026-08-14 20:33:28 +0530

    Initial project setup

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Plantation Tracking System =====");
+        System.out.println("1. Add Plant");
+        System.out.println("2. View Plants");
+        System.out.println("3. Exit");
+        System.out.println("Enter your choice:");
+
+    }
+}
